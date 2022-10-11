c02d12a2606970edba1bfa41248cd7a2e7e9f2c0
Choi Dasom <dasomchoi52@gmail.com>
2022-10-12 00:00:03 +0900

    Implement main method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         System.out.println("가장 가까운 공휴일은 언제일까요?");
         Holiday holiday = new Holiday();
-        dayholiday.findNextHoliday();
+        System.out.println(holiday.findNextHoliday().toString());
     }
 }
