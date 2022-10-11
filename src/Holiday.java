4739e7e29103ab5a4209b90f351b14ccdb039496
Choi Dasom <dasomchoi52@gmail.com>
2022-10-12 00:01:53 +0900

    Implement Holiday

--- src/Holiday.java
+++ src/Holiday.java
@@ -1,51 +1,146 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.time.LocalDate;
 
+import java.time.temporal.ChronoUnit;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Holiday {
-    LocalDate today = LocalDate.now();
-    final String serviceKey = "LkLE%2FS83ZQIzTfq1ci5gOtjHY%2FIN0gdjuaza8tWuSLW%2B8RAi9HDJOw34JxXBUbb93pfJJti8CAPtc%2FsLrBjrRA%3D%3D";
-
+    
+    final LocalDate today = LocalDate.now();
+    LocalDate calculateDay = LocalDate.now();
+    
     public DayInfo findNextHoliday() throws IOException {
-
-        while (true) {
-            /*
-            요청 url 예시
-            http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?solYear=2022&solMonth=10&ServiceKey=LkLE%2FS83ZQIzTfq1ci5gOtjHY%2FIN0gdjuaza8tWuSLW%2B8RAi9HDJOw34JxXBUbb93pfJJti8CAPtc%2FsLrBjrRA%3D%3D&_type=json
-             */
-            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getAnniversaryInfo");
-            urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + URLEncoder.encode(serviceKey, "UTF-8"));
-            urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
-            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
-            urlBuilder.append("&" + URLEncoder.encode("solYear", "UTF-8") + "=" + today.getYear()); /*연*/
-            if (today.getMonth().getValue() < 10) {
-                urlBuilder.append("&" + URLEncoder.encode("solMonth", "UTF-8") + "=" + URLEncoder.encode("0" + today.getMonth(), "UTF-8"));
-            } else {
-                urlBuilder.append("&" + URLEncoder.encode("solMonth", "UTF-8") + "=" + today.getMonth());
+        int cnt = 0; // 최대 5개월의 공휴일 정보만 조회한다
+        while (cnt < 5) {
+            cnt++;
+            URL url = makeUrl();
+            DayInfo nextHoliday = parseData(url);
+            if (nextHoliday == null) {
+                calculateDay = calculateDay.plusMonths(1);
+                calculateDay.withDayOfMonth(1);
+                continue;
             }
-            URL url = new URL(urlBuilder.toString());
+            
+            return nextHoliday;
+        }
+        return null;
+    }
+    
+    private URL makeUrl()
+        throws UnsupportedEncodingException, MalformedURLException {
+        StringBuilder urlBuilder = new StringBuilder(
+            "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo");
+        urlBuilder.append(
+            "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + Secret.serviceKey);
+        urlBuilder.append(
+            "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+        urlBuilder.append(
+            "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
+        urlBuilder.append(
+            "&" + URLEncoder.encode("solYear", "UTF-8") + "=" + calculateDay.getYear());
+        urlBuilder.append("&_type=json");
+        if (calculateDay.getMonth().getValue() < 10) {
+            urlBuilder.append(
+                "&" + URLEncoder.encode("solMonth", "UTF-8") + "=" + URLEncoder.encode(
+                    "0" + calculateDay.getMonth().getValue(), "UTF-8"));
+        } else {
+            urlBuilder.append(
+                "&" + URLEncoder.encode("solMonth", "UTF-8") + "=" + calculateDay.getMonth()
+                    .getValue());
+        }
+        
+        URL url = new URL(urlBuilder.toString());
+        
+        return url;
+    }
+    
+    
+    private DayInfo parseData(URL url) {
+        
+        String parsingData = "";
+        
+        try {
+            
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-type", "application/json");
-            System.out.println("Response code: " + conn.getResponseCode());
-            BufferedReader rd;
+            BufferedReader bf;
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                bf = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             } else {
-                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                bf = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
             }
+            
             StringBuilder sb = new StringBuilder();
             String line;
-            while ((line = rd.readLine()) != null) {
+            while ((line = bf.readLine()) != null) {
                 sb.append(line);
             }
-            rd.close();
+            
+            bf.close();
             conn.disconnect();
-            System.out.println(sb.toString());
-
-            today = today.plusMonths(1);
+            
+            parsingData = sb.toString();
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(parsingData);
+            JSONObject response = (JSONObject) jsonObject.get("response");
+            JSONObject body = (JSONObject) response.get("body");
+            
+            if (body.get("totalCount").toString().equals("0")) { // 해당 월에 공휴일이 없는 경우
+                return null;
+            }
+            JSONObject items = (JSONObject) body.get("items");
+            
+            JSONArray item = new JSONArray();
+            if (items.get("item") instanceof JSONObject) { // item의 type이 JSONObject인 경우
+                item.add(items.get("item"));
+            } else { // item의 type이 JSONArray인 경우
+                item = (JSONArray) items.get("item");
+            }
+            
+            for (int i = 0; i < item.size(); i++) {
+                JSONObject o = (JSONObject) item.get(i);
+                
+                if (o.get("isHoliday").equals("N")) {
+                    continue;
+                }
+                
+                String stringDate = String.valueOf(o.get("locdate"));
+                LocalDate date = calculateDate(stringDate);
+                if (date.isBefore(calculateDay) || date.isEqual(calculateDay)) {
+                    continue;
+                }
+                
+                DayInfo result = DayInfo.builder()
+                    .dateName((String) o.get("dateName"))
+                    .year(date.getYear())
+                    .month(date.getMonth().getValue())
+                    .day(date.getDayOfMonth())
+                    .remainingDays((int) ChronoUnit.DAYS.between(today, date))
+                    .build();
+                
+                return result;
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        
+        return null;
+    }
+    
+    LocalDate calculateDate(String date) {
+        LocalDate result;
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(4, 6));
+        int day = Integer.parseInt(date.substring(6, 8));
+        
+        return LocalDate.of(year, month, day);
     }
 }
